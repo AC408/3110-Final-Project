aa95681d484dde0644d2146756799c51dab4bc56
pujita-sunder <98840104+pujita-sunder@users.noreply.github.com>
2022-03-06 17:53:56 -0500

    Update piece.mli

--- piece.mli
+++ piece.mli
@@ -17,8 +17,8 @@ val get_color (piece -> color)
 (** [get_position p] returns the position of the piece [p]. *)
 val get_position (piece -> (char, int) option)
 
-(** [place_piece pos c l] returns the posiiton [pos], color [c] and level [l] of the function. *)
-val place_piece pos c l (unknown -> unknown)
+(** [place_piece pos c l] returns the position [pos], color [c] and level [l] of the function. *)
+val place_piece pos c l ( (char, int) option -> color -> level -> piece )
 
 (** [move_piece p pos] moves the piece [p] to position [pos]. *)  
 val move_piece p pos 
