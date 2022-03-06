1bb2ee28c1664f7990c4ba7ed50bd2e61d632350
pujita-sunder <98840104+pujita-sunder@users.noreply.github.com>
2022-03-06 17:13:59 -0500

    Update piece.ml

--- piece.ml
+++ piece.ml
@@ -1,1 +1,12 @@
 type level = Pawn | Knight | Bishop | Rook | Queen | King
+type color = White | Black
+type position = (char, int)
+
+type piece {
+  position : position
+  color : color
+  level : level
+  unused : bool
+ }
+
+
