251c125f12332ffa9b8326fb350a77d7282b8963
Allen Chen <ac2324@cornell.edu>
2022-03-06 18:22:35 -0500

    added loose files to proj structure
    fixed some syntax errs

--- terminal_chess/src/piece.ml
+++ terminal_chess/src/piece.ml
@@ -1,10 +1,10 @@
 type level = Pawn | Knight | Bishop | Rook | Queen | King
 type color = White | Black
-type position = (char, int) option
+type position = (char * int) option
 
 type piece = {
-  position : position
-  color : color
+  position : position;
+  color : color;
   level : level
  }
 
@@ -13,11 +13,11 @@ let get_color p = p.color
 let get_position p = p.position
 
 let place_piece pos c l = {
-  position = pos
-  color = c
+  position = pos;
+  color = c;
   level = l
  }
   
 let move_piece p pos =
-  place_piece pos p.c p.l
+  place_piece pos p.color p.level
 
