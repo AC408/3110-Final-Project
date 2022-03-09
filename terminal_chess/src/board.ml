de873455ebc688dce82f6724a86f8c82e6320ffb
Pujita Sunder <ps779@cornell.edu>
2022-03-08 19:55:56 -0500

    Added update_turn, which increments the number of moves and changes the player color whose turn it is.

--- terminal_chess/src/board.ml
+++ terminal_chess/src/board.ml
@@ -3,7 +3,7 @@ open Piece
 type t = piece list
 
 type model = {
- moves : int
+ moves : int;
  turn : color
 }
 
@@ -17,45 +17,47 @@ type change = Change
 let update_turn (board : t) (change : change) =
   match change with
   | Change -> 
-    let turn =
+    let turn = begin
       match board.turn with
       | Black -> White
       | White -> Black
-    let model.moves = model.moves + 1 in {turn = turn; moves = moves}
+    end in
+    let moves = model.moves + 1 in {turn = turn; moves = moves}
 
  
 
+  let blackrook1 = {position = ('a', 8) ; color = Black ; level = Rook}
+  let blackknight1 = { position = ('b', 8) ; color = Black; level = Knight}
+  let blackbishop1 = { position = ('c', 8) ; color = Black; level = Bishop}
+  let blackqueen = { position = ('d', 8) ; color = Black; level = Queen}
+  let blackking = { position = ('e', 8) ; color = Black; level = King}
+  let blackbishop2 = { position = ('f', 8) ; color = Black; level = Bishop}
+  let blackknight2 = { position = ('g', 8) ; color = Black; level = Knight}
+  let blackrook2 = { position = ('h', 8) ; color = Black; level = Rook}
+  let blackpawn1 = { position = ('a', 7) ; color = Black; level = Pawn}
+  let blackpawn2 = { position = ('b', 7) ; color = Black; level = Pawn}
+  let blackpawn3 = { position = ('c', 7) ; color = Black; level = Pawn}
+  let blackpawn4 = { position = ('d', 7) ; color = Black; level = Pawn}
+  let blackpawn5 = { position = ('e', 7) ; color = Black; level = Pawn}
+  let blackpawn6 = { position = ('f', 7) ; color = Black; level = Pawn}
+  let blackpawn7 = { position = ('g', 7) ; color = Black; level = Pawn}
+  let blackpawn8 = { position = ('h', 7) ; color = Black; level = Pawn}
+  let whiterook1 = { position = ('a', 1) ; color = White; level = Rook}
+  let whiteknight1 = { position = ('b', 1) ; color = White; level = Knight}
+  let whitebishop1 = { position = ('c', 1) ; color = White; level = Bishop}
+  let whitequeen = { position = ('d', 1) ; color = White; level = Queen}
+  let whiteking = { position = ('e', 1) ; color = White; level = King}
+  let whitebishop2 = { position = ('f', 1) ; color = White; level = Bishop}
+  let whiteknight2 = { position = ('g', 1) ; color = White; level = Knight}
+  let whiterook2 = { position = ('h', 1) ; color = White; level = Rook}
+  let whitepawn1 = { position = ('a', 2) ; color = White; level = Pawn}
+  let whitepawn2 = { position = ('b', 2) ; color = White; level = Pawn}
+  let whitepawn3 = { position = ('c', 2) ; color = White; level = Pawn}
+  let whitepawn4 = { position = ('d', 2) ; color = White; level = Pawn}
+  let whitepawn5 = { position = ('e', 2) ; color = White; level = Pawn}
+  let whitepawn6 = { position = ('f', 2) ; color = White; level = Pawn}
+  let whitepawn7 = { position = ('g', 2) ; color = White; level = Pawn}
+  let whitepawn8 = { position = ('h', 2) ; color = White; level = Pawn}
 let init_board = [
-  blackrook1 = { position = ('a', 8) ; color = Black ; level = Rook}
-  blackknight1 = { position = ('b', 8) ; color = Black; level = Knight}
-  blackbishop1 = { position = ('c', 8) ; color = Black; level = Bishop}
-  blackqueen = { position = ('d', 8) ; color = Black; level = Queen}
-  blackking = { position = ('e', 8) ; color = Black; level = King}
-  blackbishop2 = { position = ('f', 8) ; color = Black; level = Bishop}
-  blackknight2 = { position = ('g', 8) ; color = Black; level = Knight}
-  blackrook2 = { position = ('h', 8) ; color = Black; level = Rook}
-  blackpawn1 = { position = ('a', 7) ; color = Black; level = Pawn}
-  blackpawn2 = { position = ('b', 7) ; color = Black; level = Pawn}
-  blackpawn3 = { position = ('c', 7) ; color = Black; level = Pawn}
-  blackpawn4 = { position = ('d', 7) ; color = Black; level = Pawn}
-  blackpawn5 = { position = ('e', 7) ; color = Black; level = Pawn}
-  blackpawn6 = { position = ('f', 7) ; color = Black; level = Pawn}
-  blackpawn7 = { position = ('g', 7) ; color = Black; level = Pawn}
-  blackpawn8 = { position = ('h', 7) ; color = Black; level = Pawn}
-  whiterook1 = { position = ('a', 1) ; color = White; level = Rook}
-  whiteknight1 = { position = ('b', 1) ; color = White; level = Knight}
-  whitebishop1 = { position = ('c', 1) ; color = White; level = Bishop}
-  whitequeen = { position = ('d', 1) ; color = White; level = Queen}
-  whiteking = { position = ('e', 1) ; color = White; level = King}
-  whitebishop2 = { position = ('f', 1) ; color = White; level = Bishop}
-  whiteknight2 = { position = ('g', 1) ; color = White; level = Knight}
-  whiterook2 = { position = ('h', 1) ; color = White; level = Rook}
-  whitepawn1 = { position = ('a', 2) ; color = White; level = Pawn}
-  whitepawn2 = { position = ('b', 2) ; color = White; level = Pawn}
-  whitwpawn3 = { position = ('c', 2) ; color = White; level = Pawn}
-  whitepawn4 = { position = ('d', 2) ; color = White; level = Pawn}
-  whitepawn5 = { position = ('e', 2) ; color = White; level = Pawn}
-  whitepawn6 = { position = ('f', 2) ; color = White; level = Pawn}
-  whitepawn7 = { position = ('g', 2) ; color = White; level = Pawn}
-  whitepawn8 = { position = ('h', 2) ; color = White; level = Pawn}
+
 ]
