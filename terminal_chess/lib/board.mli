169da5c1e6b8d7efa058c02274b195ff88e8a4a7
Pujita Sunder <ps779@cornell.edu>
2022-03-15 21:24:21 -0400

    specs for board.mli

--- terminal_chess/lib/board.mli
+++ terminal_chess/lib/board.mli
@@ -1,16 +1,21 @@
+(** functions to update a board and specific pieces that go on the board*)
 
-
+(**representation of the current board*)
 type model = {
   moves : int;
   turn : Piece.color
  }
 
+(**model value of the starting board of each game*)
 val model_move : model
 
+(**sends a message to the model to update the color and number of moves*)
 type change = Change
 
+(** changes the player turn from White to Black (or vice versa). Adds 1 to the number of moves.*)
 val update_turn : model -> change -> model
 
+(**Representations of all the pieces that are found on the starting board before any captures*)
 val blackrook1 : Piece.piece
 val blackknight1 : Piece.piece
 val blackbishop1 : Piece.piece
