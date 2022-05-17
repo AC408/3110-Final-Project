334013bc24de572a58d9ba35da2f4ac41f471610
annapaisleygray80 <101069917+annapaisleygray80@users.noreply.github.com>
2022-05-17 15:48:03 -0400

    Update piece.ml

--- terminal_chess/lib/piece.ml
+++ terminal_chess/lib/piece.ml
@@ -1,3 +1,5 @@
+open Yojson.Basic.Util
+
 type level =
   | Pawn
   | Knight
@@ -22,6 +24,30 @@ type piece = {
   rep : string;
   moved : bool;
 }
+
+type t = { pieces : piece list }
+
+(** to go in board.ml: let piecesjson = Yojson.Basic.from_file
+    "pieces.json" let board = from_json piecesjson *)
+
+let from_json j =
+  { pieces = j |> member "pieces" |> to_list |> List.map piece_of_json }
+
+let position_of_json j =
+  {
+    row = j |> member "row" |> to_string;
+    col = j |> member "col" |> to_int;
+  }
+
+let piece_of_json j =
+  {
+    name = j |> member "name" |> to_string;
+    position = j |> member "position" |> List.map position_of_json;
+    color = j |> member "color" |> to_string;
+    level = j |> member "level" |> to_string;
+    rep = j |> member "rep" |> to_string;
+    moved = j |> member "moved" |> to_bool;
+  }
 
 let rep space =
   match space with
